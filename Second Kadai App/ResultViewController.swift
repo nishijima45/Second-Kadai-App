0a9a3545a5dff198a3cd4fe2a56e1cdc386fa551
西嶋信吾 <tree-frog@live.jp>
2018-04-26 20:59:47 +0900

    ボタンで画面の行き来ができるようになった。 シミュレータが起動しない

---
+++ Second Kadai App/ResultViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ResultViewController.swift
+//  Second Kadai App
+//
+//  Created by 西嶋 信吾 on 2018/04/25.
+//  Copyright © 2018年 西嶋 信吾. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
+    @IBOutlet weak var label: UILabel!
+    
+    // 受け取るためのプロパティ（変数）を宣言しておく
+    var textField:String = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        
+        let result = textField
+        label.text = "こんにちは \(result) さん"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Second Kadai App/ViewController.swift
+++ Second Kadai App/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているtextFieldに値を代入して渡す
 
-
+        resultViewController.textField = "self.textField.text"
+        
+    }
 }
-
